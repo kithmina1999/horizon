4d4a559f83bbdc6dadd5c9d3ccf45430daac3c66
kithmina1999 <124335388+kithmina1999@users.noreply.github.com>
2024-08-04 07:37:07 +0530

    implemet complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,25 @@
-
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Kith', lastName: 'Gun' };
   return (
-    <main>
-        {/* Sidebar */}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col ">
+        <div className="root-layout">
+          <Image src='/icons/logo.svg' alt="menu icon" height={30} width={30} />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = {firstName:'Kith',lastName:'Gun',email:'kith@gmail.com'};
+    
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className="home-content">
+            <header className='home-header'>
+                <HeaderBox 
+                    type='greeting'
+                    title="Welcome"
+                    user={loggedIn?.firstName||'Guest'}
+                    subtext="Access and manage your account and transactions efficiently"
+                />
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBank={1}
+                    totalCurrentBalance = {1250.35}
+                />
+            </header>
+            Recent Transactions
+        </div>
+
+        <RightSideBar user={loggedIn} transactions={[]} banks={[{currentBalance:123.50},{currentBalance:500.50}]}/>
+    </section>
   )
 }
 
