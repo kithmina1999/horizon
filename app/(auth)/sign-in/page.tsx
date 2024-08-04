4796c800adf35215d998ce805ad9eb0a5b4c6a17
kithmina1999 <124335388+kithmina1999@users.noreply.github.com>
2024-08-04 10:59:35 +0530

    implement auth ui and handle validations with zod

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const signIn = () => {
+const SignIn = () => {
   return (
-    <div>signIn</div>
+    <section className='flex-center size-full max-sm:px-6 '>
+        <AuthForm type='sign-in' />
+    </section>
   )
 }
 
-export default signIn
+export default SignIn
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,17 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type:string) => z.object({
+  //both
+  email: z.string().email(),
+  password: z.string().min(8),
+  //sign-up
+  firstname: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastname: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+  postalcode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBisth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+})
